e28f306343bc8db4cf3fc493d43bf790f90f9eef
cieroxelek <121301071+cieroxelek@users.noreply.github.com>
2026-02-11 02:54:25 +0100

    removed illegal hooks

--- src/App.jsx
+++ src/App.jsx
@@ -114,6 +114,7 @@ function App() {
   const [decision, setDecision] = useState(0)
   const [ready, setReady] = useState(0)
   const frames = [frame0,frame1,frame2,frame3,frame4,frame5,frame6,frame7,frame8,frame9,frame10,frame11,frame12,frame13,frame14,frame15,frame16,frame17,frame18,frame19,frame20,frame21,frame22,frame23,frame24];
+  let currentFrame = FrameFunction(count,decision);
   useEffect(() => {
     preloadSvgs(frames)
     .then(() => setReady(1))
@@ -123,7 +124,7 @@ function App() {
     ready&&<>
       <div>
         <a>
-          <img src={frames[FrameFunction(count,decision)]} className="logo react" alt="piękny piesek, szkoda że nie widzisz" />
+          <img src={frames[currentFrame]} className="logo react" alt="piękny piesek, szkoda że nie widzisz" />
         </a>
       </div>
       <h1>CZY ZOSTANIESZ MOJĄ WALENTYNKĄ?</h1>
